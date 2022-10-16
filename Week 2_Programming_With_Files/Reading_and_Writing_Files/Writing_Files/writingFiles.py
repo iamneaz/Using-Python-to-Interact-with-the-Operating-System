d7b3657a1ca2ef18d02549adb051abe58194689b
iamneaz <neazahmedneaz@gmail.com>
2022-10-16 13:36:35 +0600

    Reading and Writing Files added

---
+++ Week 2_Programming_With_Files/Reading_and_Writing_Files/Writing_Files/writingFiles.py
@@ -0,0 +1,7 @@
+filepath = r"novel.txt"
+with open(filepath, "w") as file:
+    file.write("It was a dark and stormy night")
+# r for read only
+# w for write only
+# a for append
+# r+ for read - write
