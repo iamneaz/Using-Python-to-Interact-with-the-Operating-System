5bdbdfc6dc093e79c3ade10cebd0caa74dcabe82
iamneaz <neazahmedneaz@gmail.com>
2022-10-31 22:48:08 +0600

    started pipes and pipelines

---
+++ Week_6_Bash_Scripting/1.Interacting_with_the_command_line_shell/3.Redirecting_Streams/redirecting_streams.sh
@@ -0,0 +1,7 @@
+#   REDIRECTION 
+#   --  the process of sending a stream to a different destination
+./stdout.py > stdout.txt
+#   Each time we perform a redirection of STDOUT the destination is overwritten
+#   to append we use a >> sign
+./stdout.py >> stdout.txt
+
---
+++ Week_6_Bash_Scripting/1.Interacting_with_the_command_line_shell/3.Redirecting_Streams/streams_err.py
@@ -0,0 +1,3 @@
+data = input("This will come from STDIN:")
+print("Now we write it to STDOUT: " + data)
+raise ValueError("now we generate a error to STDERROR")
